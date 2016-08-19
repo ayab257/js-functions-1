f2e3ce7f81403d56a0616d8dcc91821c333ec840
ayab257 <ayab257@gmail.com>
2016-08-19 10:23:32 +0300

    second

--- js/script.js
+++ js/script.js
@@ -1,6 +1,14 @@
  function myFunction() {
         var y = document.getElementById("txt1").value;
         var z = document.getElementById("txt2").value;
-        var x = +y + +z;
-        document.getElementById("demo").innerHTML = x;
+        var demo = document.getElementById("demo");
+        if (+y > +z){
+        	demo.innerHTML = y + "  is bigger";
+        }
+        else{
+        	demo.innerHTML = z+ " is bigger";
+        }
+        
+        // var x = +y + +z;
+        // document.getElementById("demo").innerHTML = x;
 }
